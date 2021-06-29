7b60f5b3e6a65079ae8b88af75d52bb7b5d1b0e2
Vardhan Rajnikant Shah <vardhan.s@ahduni.edu.in>
2021-06-29 15:13:26 +0530

    The Messagrs has been succesfully displayed but it we can't manually close it fades after 10 sec.

--- crudproject1/crudproject1/settings.py
+++ crudproject1/crudproject1/settings.py
@@ -133,11 +133,11 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 MESSAGE_TAGS = {
-    messages.DEBUG: "alert-secondary",
-    messages.INFO: "alert-info",
-    messages.SUCCESS: "alert-success",
-    messages.WARNING: "alert-warning",
-    messages.ERROR: "alert-danger",
+    messages.DEBUG: "secondary",
+    messages.INFO: "info",
+    messages.SUCCESS: "success",
+    messages.WARNING: "warning",
+    messages.ERROR: "danger",
 }
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
--- crudproject1/enroll/views.py
+++ crudproject1/enroll/views.py
@@ -29,7 +29,7 @@ def login_request(request):
                 user = authenticate(username=username, password=password)
                 if user is not None:
                     login(request, user)
-                    messages.info(request, "You are now logged in as {username}.")
+                    messages.info(request, "You are now logged in as " + username + ".")
                     return HttpResponseRedirect("/view_products")
                 else:
                     messages.error(request, "Invalid username or password.")
